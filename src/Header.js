168eba0435a37327383aedf40964342c02e6cdf4
= <=>
2019-06-11 13:51:53 -0400

    Working on header component/ Logo in progress

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    state = {
+        mode: 'visible'
+    }
+    render() {
+        return (
+            
+        )
+    }
+}
