aa86b7a4dbe16a65f086294ba8768f27e8c56c3f
= <=>
2019-06-13 17:23:44 -0400

    Working on some components for the header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-    state = {
-        mode: 'visible'
-    }
-    render() {
-        return (
-            
-        )
-    }
+  state = {
+    mode: "visible"
+  };
+
+  render() {
+    return (
+      <div className="AppHeader">
+        <nav>
+          <h1 className="AppLogo"> fgd</h1>
+        </nav>
+      </div>
+    );
+  }
 }
+
+export default Header;
