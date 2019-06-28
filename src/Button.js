d7af7e3a37fc3d818448c3316c6155edff339572
= <=>
2019-06-28 16:21:16 -0400

    Button v1 finished

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import { Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/styles";
+
+const StyledButton = withStyles({
+  root: {
+    borderRadius: 25,
+    background: "#d50000",
+    "&:hover": {
+      background: "#dd3333"
+    },
+    // border: "2px solid #000000",
+    width: "25%",
+    height: "5%",
+    position: "absolute",
+    top: "70%",
+    left: "5%"
+  },
+  containedSecondary: {
+    color: "#ffffff"
+  },
+  sizeLarge: {
+    size: "600px"
+  }
+})(Button);
 
 class BtnHome extends Component {
   render() {
-    return <button className={this.props.type}>Test</button>;
+    return (
+      <StyledButton size="large" variant="contained" color="secondary">
+        Try it now!
+      </StyledButton>
+    );
   }
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import AppLogo from "./AppLogo";
 import Nav from "./Nav";
+import Button from "./Button";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/styles";
+
+const StyledText = withStyles({
+  root: {
+    color: "#2137b1",
+    fontSize: "2.5em",
+    fontFamily: "Press Start 2P"
+  }
+})(Typography);
 
 class Header extends Component {
   render() {
@@ -9,6 +20,8 @@ class Header extends Component {
         <div className="HeaderBackground-figure">
           <AppLogo />
           <Nav />
+          <StyledText>ALIVEVEVEVEV</StyledText>
+          <Button />
         </div>
       </header>
     );
