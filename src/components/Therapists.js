f4cc106696d76a7fa75c76221565cd20747c92c9
Cindy Cuevas <cindycuevas@Cindys-MacBook-Air.local>
2019-07-05 08:52:04 -0400

    Mr. Krabs i had an idea about the thumbnails

--- src/components/Therapists.js
+++ src/components/Therapists.js
@@ -23,75 +23,75 @@ const cardStyles = withStyles({
 })(Card);
 
 class Therapists extends Component {
-  state = {
-    currentData: null,
-    name: "",
-    sprite: "",
-    url: "",
-    description: ""
-  };
-
   fetchInfo(URI) {
-    const fetchDataPromise = new Promise(async function(successful, rejected) {
-      await axios
+    return new Promise(resolve => {
+      axios
         .get(URI)
         .then(response => {
           const data = response.data.results;
-          this.setState({
-            currentData: data
-          });
-          successful(this.state.currentData);
+          resolve(data);
         })
         .catch(error => {
           console.error(error);
-          rejected(error);
         });
     });
   }
+
+  // showData(apiDataNames, ) {
+  //   <div>
+  //     {apiData.map(pokemon => (
+  //       <cardStyles>
+  //         <CardActionArea>
+  //           {console.log(apiData)}
+  //           <mediaStyles image={} title={} />
+  //           <CardContent>
+  //             <Typography gutterBottom variant="h5" component="h2">
+  //               {}
+  //             </Typography>
+  //             <Typography variant="body2" color="textSecondary" component="p">
+  //               {}
+  //             </Typography>
+  //           </CardContent>
+  //         </CardActionArea>
+  //         <CardActions>
+  //           <Button size="small" color="primary">
+  //             Share
+  //           </Button>
+  //           <Button size="small" color="primary">
+  //             Learn More
+  //           </Button>
+  //         </CardActions>
+  //       </cardStyles>
+  //     ))}
+  //   </div>;
+  // }
 
   render() {
     return (
       <div>
         {this.fetchInfo("https://pokeapi.co/api/v2/pokemon")
           .then(response => {
-            response(console.log(response.successful));
+            let pokemonName = response.name;
+            let pokemonImg = null;
+            let pokemonDescription = null;
+            response.map(
+              pokemon => (
+                (pokemonName = pokemon.name),
+                this.fetchInfo(pokemon.url).then(result => {
+                  let pokemonImg = result.sprites.front_default;
+                  this.fetchInfo(result.species.url).then(resultSpecies => {
+                    let pokemonDescription =
+                      resultSpecies.flavor_text_entries[1].flavor_text;
+                  });
+                })
+              )
+            );
           })
           .catch(error => {
-            console.log(error.rejected);
+            console.log(error);
           })}
-        {this.state.currentData ? <h1>ponisalvaje</h1> : <CircularProgress />}
       </div>
     );
-    // <div>
-    //   {this.props.map(pokemon => (
-    //     <cardStyles>
-    //       <CardActionArea>
-    //         {console.log("working")}
-    //         {console.log(this.fetchInfo("https://pokeapi.co/api/v2/pokemon"))}
-    //         <mediaStyles
-    //           image={this.props["url"]}
-    //           title={this.props["name"]}
-    //         />
-    //         <CardContent>
-    //           <Typography gutterBottom variant="h5" component="h2">
-    //             {this.props["name"]}
-    //           </Typography>
-    //           <Typography variant="body2" color="textSecondary" component="p">
-    //             {this.props["url"]}
-    //           </Typography>
-    //         </CardContent>
-    //       </CardActionArea>
-    //       <CardActions>
-    //         <Button size="small" color="primary">
-    //           Share
-    //         </Button>
-    //         <Button size="small" color="primary">
-    //           Learn More
-    //         </Button>
-    //       </CardActions>
-    //     </cardStyles>
-    //   ))}
-    // </div>
   }
 }
 
